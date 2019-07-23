e91469c70fff940d877dd6bfea7070b00439e664
admin <425359983@qq.com>
2019-07-24 00:19:52 +0800

    fix

--- src/main/java/com/sunney/test/MyProducder.java
+++ src/main/java/com/sunney/test/MyProducder.java
@@ -1,26 +1,40 @@
 package com.sunney.test;
 
+
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 
-
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.Properties;
+import java.util.Random;
 
 public class MyProducder {
 
-    public MyProducder() {
-        Map properties = new HashMap<String,String>();
-        properties.put("brok.list", "localhost:9092");
-        properties.put("serializer.class", "kafka.serializer.StringEncoder");
-        properties.put("request,reqired.acks", "1");
-        Producer<Integer, String> producer = new KafkaProducer(properties);
+    /**
+     * 定义主题
+     */
+    public static String topic = "duanjt_test";
 
-        ProducerRecord<Integer, String> msg = new ProducerRecord<>("test", 100, "hello word");
-        producer.send(msg);
-        System.out.println("ok");
+    public static void main(String[] args) throws InterruptedException {
+        Properties p = new Properties();
+        //kafka地址，多个地址用逗号分割
+        p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.23.76:9092");
+        p.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        p.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(p);
+
+        try {
+            while (true) {
+                String msg = "Hello," + new Random().nextInt(100);
+                ProducerRecord<String, String> record = new ProducerRecord<>(topic, msg);
+                kafkaProducer.send(record);
+                System.out.println("消息发送成功:" + msg);
+                Thread.sleep(500);
+            }
+        } finally {
+            kafkaProducer.close();
+        }
 
     }
 }
