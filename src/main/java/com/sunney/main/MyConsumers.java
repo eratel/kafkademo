3a6e2c4ec0cd5788caa1e1099306c04725fe4739
admin <425359983@qq.com>
2019-07-27 11:51:17 +0800

    11

--- src/main/java/com/sunney/main/MyConsumers.java
+++ src/main/java/com/sunney/main/MyConsumers.java
@@ -18,13 +18,15 @@ public class MyConsumers {
     public static void main(String[] args) {
         Properties props = new Properties();
         String TOPIC = "test10";
-        String GROUP = "name";
+        String GROUP = "g1";
 
         props.put("bootstrap.servers", "192.168.40.128:9092");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         //消费者组的名称
         props.put("group.id", GROUP);
+        //从头开始消费
+        props.put("auto.offset.reset", "earliest");
 
         KafkaConsumer consumer = new KafkaConsumer(props);
         consumer.subscribe(Arrays.asList(TOPIC));
